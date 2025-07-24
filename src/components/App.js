b6c129ad33b184b7726f6d8affd96496656bc8d4
alexis <alexis.faivret@gmail.com>
2025-07-24 14:43:26 +0200

    banner et navbar creer

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../styles/App.css';
+import Banner from './Banner';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <>
+        <Banner />
+        <Navbar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import '../styles/Navbar.css';
+
+function Navbar() {
+    return (
+        <nav>
+            <a href='Accueil'>Accueil</a>
+            <a href='Profil'>Profil</a>
+            <a href='Competences'>Compétences</a>
+            <a href='Portfolio'>Portfolio</a>
+            <a href='Contact'>Contact</a>
+        </nav>
+    )
+}
+
+export default Navbar
