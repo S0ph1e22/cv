ef604a275a6a3d6c0d318e4791ed9661b5614f85
Sophie <Sophie@sfr.fr>
2025-08-01 18:35:43 +0200

    animation competence ok

--- src/Competences/ProgressBarre.js
+++ src/Competences/ProgressBarre.js
@@ -1,11 +1,33 @@
 import '../Competences/ProgressBarre.css';
+import { useEffect, useRef, useState } from 'react';
 
 function ProgressBarre({label, percent}) {
+    const [visible, setVisible] = useState(false);
+    const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setVisible(true);
+          observer.disconnect(); 
+        }
+      },
+      { threshold: 0.3 } 
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
+
     return(
-        <div className="progressbarre">
+        <div className="progressbarre"ref={ref}>
             <span className='progress-name'> {label} </span>
             <div className='progress-barre'>
-                <div className='progress-fill' style={{ width: `${percent}%`}}>
+                <div className='progress-fill' style={{ width: visible ? `${percent}%` : '0%' }}>
                      <span className='progress-text'>{percent}%</span>
                 </div>
                
